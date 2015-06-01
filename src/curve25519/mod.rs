6de85c08a61c9f02e55c0ddd5534663130c9b32e
Mikael Voss <mvs@nyantec.com>
2015-06-01 20:58:40 +0200

    curve25519: Add field element type

--- src/curve25519/mod.rs
+++ src/curve25519/mod.rs
@@ -1,4 +1,5 @@
 use std::cmp::{Eq, PartialEq};
+use std::ops::{Add,Sub,Mul};
 
 /// Curve25519 scalar
 #[derive(Clone, Copy)]
@@ -22,6 +23,9 @@ pub static BASE: Point = Point([
 	0, 0, 0, 0, 0, 0, 0, 0
 ]);
 
+/// Curve25519 field element
+struct Element([i64; 10]);
+
 impl PartialEq for Scalar {
 	fn eq(&self, other: &Self) -> bool {
 		let &Scalar(lhs) = self;
@@ -41,3 +45,47 @@ impl PartialEq for Point {
 }
 
 impl Eq for Point {}
+
+impl PartialEq for Element {
+	fn eq(&self, other: &Self) -> bool {
+		let &Element(lhs) = self;
+		let &Element(rhs) = other;
+		lhs == rhs
+	}
+}
+
+impl Eq for Element {}
+
+impl Add for Element {
+	type Output = Element;
+
+	fn add(self, other: Element) -> Self::Output {
+		let &Element(lhs) = &self;
+		let &Element(rhs) = &other;
+
+		Element([
+			lhs[0] + rhs[0], lhs[1] + rhs[1],
+			lhs[2] + rhs[2], lhs[3] + rhs[3],
+			lhs[4] + rhs[4], lhs[5] + rhs[5],
+			lhs[6] + rhs[6], lhs[7] + rhs[7],
+			lhs[8] + rhs[8], lhs[9] + rhs[9],
+		])
+	}
+}
+
+impl Sub for Element {
+	type Output = Element;
+
+	fn sub(self, other: Element) -> Self::Output {
+		let &Element(lhs) = &self;
+		let &Element(rhs) = &other;
+
+		Element([
+			lhs[0] - rhs[0], lhs[1] - rhs[1],
+			lhs[2] - rhs[2], lhs[3] - rhs[3],
+			lhs[4] - rhs[4], lhs[5] - rhs[5],
+			lhs[6] - rhs[6], lhs[7] - rhs[7],
+			lhs[8] - rhs[8], lhs[9] - rhs[9],
+		])
+	}
+}
