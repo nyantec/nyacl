0d84565a867c3c7ea6ad9f54874221f6da405177
Mikael Voss <mvs@nyantec.com>
2015-06-01 18:09:59 +0200

    curve25519: Add type aliases for keys

--- src/curve25519/mod.rs
+++ src/curve25519/mod.rs
@@ -8,6 +8,12 @@ pub struct Scalar([u8; 32]);
 #[derive(Clone, Copy)]
 pub struct Point([u8; 32]);
 
+/// Curve25519 secret key
+pub type SecretKey = Scalar;
+
+/// Curve25519 public key
+pub type PublicKey = Point;
+
 impl PartialEq for Scalar {
 	fn eq(&self, rhs: &Self) -> bool {
 		let &Scalar(lhs_array) = self;
